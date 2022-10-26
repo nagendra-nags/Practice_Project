279a9f4c1321234de018d3afcc2f1b5e3b9ab6bd
HP <HP@DESKTOP-ESAT5E6>
2022-10-26 23:43:24 +0530

    report

--- Practice_Project/src/test/java/testScript/FlixBus.java
+++ Practice_Project/src/test/java/testScript/FlixBus.java
@@ -12,49 +12,60 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import genericUtility.BaseClass;
 import genericUtility.ExcelExample;
 import genericUtility.ExcelUtility;
 import genericUtility.FlixBusGeneric;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import pomRepositry.FlixBusPage;
 
-public class FlixBus extends FlixBusGeneric {
+public class FlixBus extends BaseClass {
 	ExcelUtility elib=new ExcelUtility();
 //	@Parameters({"round", "fromName", "toName", "pickMonth", "pickYear", "pickdate", "pickReturnMonth", "pickReturnYear", "pickReturndate", "adult", "child", "bike"})
-	@Test
-	public void bookBus(String round, String fromName, String toName, String pickMonth, String pickYear, String pickdate, String pickReturnMonth, String pickReturnYear, String pickReturndate, String adult, String child, String bike) throws InterruptedException
+	@Test(dataProvider="data")
+	public void bookBus(String round1, String fromName1, String toName1, String pickMonth1, String pickYear1, String pickdate1, String pickReturnMonth1, String pickReturnYear1, String pickReturndate1, String adult1, String child1, String bike1) throws InterruptedException
 	{
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver= new ChromeDriver();
+		
 		FlixBusPage bus=new FlixBusPage(driver);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://global.flixbus.com/");
 		
-		String round1 = System.getProperty("round");
-		bus.oneWay(round1, fromName, toName, pickMonth, pickYear, pickdate, pickReturnMonth, pickReturnYear, pickReturndate, adult, child, bike, driver);
+//		String round1 = System.getProperty("round");
+//		String fromName1 = System.getProperty("fromName");
+//		String toName1 = System.getProperty("toName");
+//		String pickMonth1 = System.getProperty("pickMonth");
+//		String pickYear1 = System.getProperty("pickYear");
+//		String pickdate1 = System.getProperty("pickdate");
+//		String pickReturnMonth1 = System.getProperty("pickReturnMonth");
+//		String pickReturnYear1 = System.getProperty("pickReturnYear");
+//		String pickReturndate1 = System.getProperty("pickReturndate");
+//		String adult1 = System.getProperty("adult");
+//		String child1 = System.getProperty("child");
+//		String bike1 = System.getProperty("bike");
+		bus.oneWay(round1, fromName1, toName1, pickMonth1, pickYear1, pickdate1, pickReturnMonth1, pickReturnYear1, pickReturndate1, adult1, child1, bike1, driver);
 		
 		
 	}
 	
-//	@DataProvider
-//	public Object[][] data() throws Throwable
-//	{
-//	
-//		Object[][] obj=new Object[1][12];
-//		obj[0][0]=elib.getdata("sheet3", 1, 0);
-//		obj[0][1]=elib.getdata("sheet3", 1, 1);
-//		obj[0][2]=elib.getdata("sheet3", 1, 2);
-//		obj[0][3]=elib.getdata("sheet3", 1, 3);
-//		
-//		obj[0][4]=elib.getdata("sheet3", 1, 4);
-//		obj[0][5]=elib.getdata("sheet3", 1, 5);
-//		obj[0][6]=elib.getdata("sheet3", 1, 6);
-//		obj[0][7]=elib.getdata("sheet3", 1, 7);
-//		obj[0][8]=elib.getdata("sheet3", 1, 8);
-//		obj[0][9]=elib.getdata("sheet3", 1, 9);
-//		obj[0][10]=elib.getdata("sheet3", 1, 10);
-//		obj[0][11]=elib.getdata("sheet3", 1, 11);
+	@DataProvider
+	public Object[][] data() throws Throwable
+	{
+	
+		Object[][] obj=new Object[1][12];
+		obj[0][0]=elib.getdata("sheet3", 1, 0);
+		obj[0][1]=elib.getdata("sheet3", 1, 1);
+		obj[0][2]=elib.getdata("sheet3", 1, 2);
+		obj[0][3]=elib.getdata("sheet3", 1, 3);
+		
+		obj[0][4]=elib.getdata("sheet3", 1, 4);
+		obj[0][5]=elib.getdata("sheet3", 1, 5);
+		obj[0][6]=elib.getdata("sheet3", 1, 6);
+		obj[0][7]=elib.getdata("sheet3", 1, 7);
+		obj[0][8]=elib.getdata("sheet3", 1, 8);
+		obj[0][9]=elib.getdata("sheet3", 1, 9);
+		obj[0][10]=elib.getdata("sheet3", 1, 10);
+		obj[0][11]=elib.getdata("sheet3", 1, 11);
 		
 //		obj[1][0]="oneway";
 //		obj[1][1]="Hamburg";
@@ -94,8 +105,8 @@ public class FlixBus extends FlixBusGeneric {
 //		obj[3][6]="3";
 //		obj[3][7]="2";
 //		obj[3][8]="1";
-//		return obj;
-//	}
+		return obj;
+	}
 	
 
 }
